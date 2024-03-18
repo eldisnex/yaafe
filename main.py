955142ffc0fb5c65b0f44e7035673855163faedb
eldisnex <105437055+eldisnex@users.noreply.github.com>
2024-03-18 19:42:03 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from time import sleep
 from s import process
 from hr import check
+import os
+
+if not os.path.isdir("Shz_1h"):
+    os.mkdir("Shz_1h")
+if not os.path.isdir("Shz_in"):
+    os.mkdir("Shz_in")
+if not os.path.isdir("Shz_out"):
+    os.mkdir("Shz_out")
+if not os.path.isdir("Shz_processed"):
+    os.mkdir("Shz_processed")
 
 while True:
     check()
